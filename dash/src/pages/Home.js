56ef85e9ea88ef5fe8ddacc9e84feb30e3df5682
mahima rathor <mahimarathor26@gmail.com>
2024-01-26 14:27:29 +0530

    first commit

---
+++ dash/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      This is Home Page
+    </div>
+  )
+}
+
+export default Home
